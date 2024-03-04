e67be71a61c2c616ec90ed36fe534509463e035d
OfirSinn <Ofir.sinn@gmail.com>
2024-03-04 21:56:48 +0200

    Quartz sync: Mar 4, 2024, 9:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 האתר של אופיר",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
