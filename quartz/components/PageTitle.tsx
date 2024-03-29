43e7b90b9b56218cae19eb5a9a31469fe6bbe38d
OfirSinn <Ofir.sinn@gmail.com>
2024-03-29 23:12:34 +0300

    Revert "Merge branch 'v4' of github.com:OfirSinn/Articles into v4"
    This reverts commit 64cb720f5d0e00f9491f508eb43dc85c9b227b67, reversing
    changes made to 68d63b129936621118ec289ef394c79dbc471f53.

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -16,12 +16,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 PageTitle.css = `
 .page-title {
   margin: 0;
-  <head>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto Serif Hebrew">
-  <style>
-	font-family: Noto Serif Hebrew;
-  </style>
-  </head>
+  font-family: Noto Serif Hebrew;
 }
 `;
 
