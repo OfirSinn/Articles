f061bc76dd981a48c0707eb59c72636c132af476
OfirSinn <81870040+OfirSinn@users.noreply.github.com>
2025-04-12 22:08:55 +0300

    Update quartz.layout.ts
    Edit theme

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,9 +17,9 @@ export const sharedPageComponents: SharedLayout = {
         category: 'Announcements',
         // from data-category-id
         categoryId: 'DIC_kwDOLZYrtc4CpCIn',
-        themeUrl: "https://example.com/static/giscus", // corresponds to quartz/static/giscus/
-        lightTheme: "light-theme", // corresponds to light-theme.css in quartz/static/giscus/
-        darkTheme: "dark-theme", // corresponds to dark-theme.css quartz/static/giscus/
+        //themeUrl: "https://example.com/static/giscus", // corresponds to quartz/static/giscus/
+        //lightTheme: "light-theme", // corresponds to light-theme.css in quartz/static/giscus/
+        //darkTheme: "dark-theme", // corresponds to dark-theme.css quartz/static/giscus/
       }
     }),
   ],
